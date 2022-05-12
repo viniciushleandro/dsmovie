a6810241189eb7cb107c7ec9cbd8dd7a5e0b03fd
Vinicius <vinilean43@gmail.com>
2022-05-12 10:40:39 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,24 @@
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
+import './styles.css';
+
+function Navbar() {
+
+    return (
+        <header>
+            <nav className='container'>
+                <div className='dsmovie-nav-content'>
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/viniciushleadro">
+                        <div className='dsmovie-contact-container'>
+                            <GithubIcon />
+                            <p className='dsmovie-contact-link'>/viniciushleadro</p>
+                        </div>
+
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
